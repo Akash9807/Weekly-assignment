5b1be96c40aa54e24047c3ec198d6a9e5f6eff04
Akash Yadav <92373612+Akash9807@users.noreply.github.com>
2022-01-12 22:18:07 +0530

    Adding UserInfo and authorization Components

--- app-frontend/src/Components/authorization/authorization.js
+++ app-frontend/src/Components/authorization/authorization.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './authorization.css'
 
 function authorization() {
     return (
-        <div>
-            
+        <div className='Container'>
+            <h2 className='Heading'>Transaction Confirmation</h2>
+            <div className="card shadow1">
+            <div className='Details'>
+                <h3>PAN Card Verification</h3>
+                <label>Enter your PAN card number</label>
+                <br />
+                <input placeholder='Enter PAN'></input>
+                <br />
+                <button>SUBMIT</button>
+            </div>
+            </div>
+            <p className='Heading'><u>Cancel Transaction</u></p>
+            <p className='Heading'>SECURE TRANSACTION</p>
+            <div className='Transaction'>
+                <img src="https://iccdn.in/img/logo_PCIDSS.svg" alt="PCIDSS security tag" class="securityTag__3YstB"></img>
+                <img src="https://iccdn.in/img/logo_PCIDSS_Copy.svg" alt="PCIDSS security tag" class="securityTagCheck__32E2m"></img>
+            </div>
         </div>
     )
 }
--- app-frontend/src/Components/userInfo/userInfo.js
+++ app-frontend/src/Components/userInfo/userInfo.js
@@ -1,11 +1,102 @@
-import React from 'react'
+import React,  { useState } from 'react';
+import "./userInfo.css";
 
-function userInfo() {
+function UserInfo() {
+
+    const [Values, setValues] = useState({
+        Amount: '',
+	    MobileNumber: '',
+    });
+
+    const handleAmountChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            Amount: event.target.value,
+        }));
+    };
+
+    const handleMobileNumberChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            MobileNumber: event.target.value,
+        }));
+    };
+    
+
     return (
-        <div>
-            
+        <div className='container'>
+            <table className='userInfo' >
+                
+                <tr>
+                    <th className='Left'>Field Description</th>
+                    <th className='right'>Field Name</th>
+                </tr>
+                <tr>
+                    <td><label>Gateway*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Merchant MID*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Currency*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Amount*</label></td>
+                    <td><input 
+                        type="text"
+                        placeholder="Amount"
+                        name="Amount"
+                        value={Values.Amount}
+                        onChange={handleAmountChange}
+                        /></td>
+                </tr>
+                <tr>
+                    <td><label>User Lang ID*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Mobile*</label></td>
+                    <td><input 
+                        type="text"
+                        placeholder="Enter Mobile Number"
+                        name="Mobile Number"
+                        value={Values.MobileNumber}
+                        onChange={handleMobileNumberChange}
+                        /></td>
+                </tr>
+                <tr>
+                    <td><label>Merchant Txn ID*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Gateway Txn ID*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Lender (Bank code)*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Cart Info*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Return URL*</label></td>
+                    <td><input/></td>
+                </tr>
+                <tr>
+                    <td><label>Secret Key*</label></td>
+                    <td><input/></td>
+                </tr>
+            </table>
+            <button className='continuebutton'>Continue</button>
         </div>
-    )
+    );
 }
 
-export default userInfo
+export default UserInfo;
